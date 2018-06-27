cdce6ba368d95b5badb450524ce161a0164b1f97
Damien Robertson <robertsondamien@gmail.com>
2018-06-27 15:47:40 -0400

    Create pydcf utils files and tests

---
+++ pydcf/utils/__init__.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+
+from scipy.optimize import curve_fit
+from numpy import \
+     min as np_min,\
+     zeros as np_zeros,\
+     mean as np_mean,\
+     loadtxt as np_loadtxt
+
+
+
+def _set_unitytime(ts1, ts2):
+
+    """
+        Subroutine - _set_unitytime
+          Simply shifts both time series so that one starts at zero.
+    """
+
+    unitytime = min(np_min(ts1[:,0]), np_min(ts2[:,0]))
+    ts1[:,0] = ts1[:,0] - unitytime
+    ts2[:,0] = ts2[:,0] - unitytime
+
+    return ts1, ts2
+
+def _validate_timeseries(ts):
+
+    """
+        Subroutine - _validate_timeseries
+          Makes sure user has entered a properly formatted timeseries.
+          Checks to see if input time series has a measurement error column - third
+          column of input file.
+    """
+
+    assert ((ts.shape[1] == 2) or (ts.shape[1] == 3)), "TS SHAPE ERROR"
+
+    if ts.shape[1] == 2:
+        ts_fill = np_zeros((ts.shape[0], 3))
+        ts_fill[:,0:2] = ts[:,0:2]
+
+        return ts_fill
+
+    else:
+
+        return ts
+
+def _tsdtrnd(ts, vrbs, plyft):
+
+    """
+        Subroutine - tsdtrnd
+          Time series detrend using the user chosen polynomial order. Subroutine
+          fits a ploynomial to the time series data and subtracts.
+    """
+
+    if plyft == 0:
+
+        ts_mean = np_mean(ts[:,1])
+        ts[:,1] = ts[:,1] - ts_mean
+        if vrbs:
+            print("Mean subtraction: %.4e" %ts_mean)
+
+    elif plyft == 1:
+
+        lnfnc = lambda x, a, b: a*x + b
+        p0, c0 = curve_fit(lnfnc, ts[:,0], ts[:,1], sigma=ts[:,2])
+        ts[:,1] = ts[:,1] - lnfnc(ts[:,0], p0[0], p0[1])
+
+        if vrbs:
+            print("Linear De-trend Coefficients [a*x + b]")
+            print("a:", p0[0])
+            print("b:", p0[1])
+
+    else:
+
+        lnfnc = lambda x, a, b, c: a*x**2.0 + b*x + c
+        p0, c0 = curve_fit(lnfnc, ts[:,0], ts[:,1], sigma=ts[:,2])
+        ts[:,1] = ts[:,1] - lnfnc(ts[:,0], p0[0], p0[1], p0[2])
+
+        if vrbs:
+            print("Quadratic De-trend Coefficients [a*x**2 + b*x + c]")
+            print("a:", p0[0])
+            print("b:", p0[1])
+            print("c:", p0[2])
+
+    return ts
+
+def _get_timeseries(infile1, infile2, vrbs, plyft):
+
+    """
+        Subroutine - get_timeseries
+          Takes the user specified filenames and runs tsdtrnd and set_unitytime.
+          Returns the prepared time series for DCF.
+    """
+
+    ts1_in = np_loadtxt(infile1, comments='#', delimiter=',')
+    ts2_in = np_loadtxt(infile2, comments='#', delimiter=',')
+
+    ts1 = chck_tserr(ts1_in)
+    ts2 = chck_tserr(ts2_in)
+
+    ts1, ts2 = _set_unitytime(ts1, ts2)
+    ts1 = tsdtrnd(ts1, vrbs, plyft)
+    ts2 = tsdtrnd(ts2, vrbs, plyft)
+
+    return ts1, ts2
---
+++ tests/utils/test_utils.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+
+from numpy import array as np_array, zeros as np_zeros
+import pytest
+
+from pydcf.utils import _set_unitytime, _validate_timeseries
+
+
+@pytest.fixture
+def test_ts1():
+    return np_array([
+        [11., 1., 0.1],
+        [13., 3., 0.1],
+        [14., 4., 0.1],
+    ])
+
+
+@pytest.fixture
+def test_ts2():
+    return np_array([
+        [21., 1., 0.1],
+        [22., 2., 0.1],
+        [24., 4., 0.1],
+    ])
+
+
+@pytest.fixture
+def array_equality_test_helper(expected_ts, output_ts):
+    return all(a == b for row_a, row_b in zip(expected_ts, output_ts) for a, b in zip(row_a, row_b))
+
+
+def test__set_unitytime(test_ts1, test_ts2):
+    expected_ts1 = np_array([
+        [0., 1., 0.1],
+        [2., 3., 0.1],
+        [3., 4., 0.1],
+    ])
+
+    expected_ts2 = np_array([
+        [10., 1., 0.1],
+        [11., 2., 0.1],
+        [13., 4., 0.1],
+    ])
+
+    output_ts1, output_ts2 = _set_unitytime(test_ts1, test_ts2)
+
+    assert array_equality_test_helper(expected_ts1, output_ts1)
+    assert array_equality_test_helper(expected_ts2, output_ts2)
+
+
+def test__validate_timeseries(test_ts1):
+    test_ts_2_col = test_ts1[:, :2]
+    test_ts_4_col = np_zeros((test_ts1.shape[0], 4))
+
+    expected_test_ts_2_col = np_zeros((test_ts1.shape[0], 3))
+    expected_test_ts_2_col[:, :2] = test_ts_2_col
+
+    assert array_equality_test_helper(_validate_timeseries(test_ts1), test_ts1)
+    assert array_equality_test_helper(_validate_timeseries(test_ts_2_col), expected_test_ts_2_col)
