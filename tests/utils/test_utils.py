da3621c2ed3fa32035d0f94679e8a4e39020ac59
Damien Robertson <robertsondamien@gmail.com>
2018-06-28 00:17:36 -0400

    Updated names in utils tests

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from numpy import array as np_array, zeros as np_zeros
 import pytest
 
-from pydcf.utils import _set_unitytime, _validate_timeseries
+from pydcf.utils import set_unitytime, validate_timeseries
 
 
 @pytest.fixture
@@ -29,7 +29,7 @@ def array_equality_test_helper(expected_ts, output_ts):
     return all(a == b for row_a, row_b in zip(expected_ts, output_ts) for a, b in zip(row_a, row_b))
 
 
-def test__set_unitytime(test_ts1, test_ts2):
+def test_set_unitytime(test_ts1, test_ts2):
     expected_ts1 = np_array([
         [0., 1., 0.1],
         [2., 3., 0.1],
@@ -42,18 +42,18 @@ def test__set_unitytime(test_ts1, test_ts2):
         [13., 4., 0.1],
     ])
 
-    output_ts1, output_ts2 = _set_unitytime(test_ts1, test_ts2)
+    output_ts1, output_ts2 = set_unitytime(test_ts1, test_ts2)
 
     assert array_equality_test_helper(expected_ts1, output_ts1)
     assert array_equality_test_helper(expected_ts2, output_ts2)
 
 
-def test__validate_timeseries(test_ts1):
+def test_validate_timeseries(test_ts1):
     test_ts_2_col = test_ts1[:, :2]
     test_ts_4_col = np_zeros((test_ts1.shape[0], 4))
 
     expected_test_ts_2_col = np_zeros((test_ts1.shape[0], 3))
     expected_test_ts_2_col[:, :2] = test_ts_2_col
 
-    assert array_equality_test_helper(_validate_timeseries(test_ts1), test_ts1)
-    assert array_equality_test_helper(_validate_timeseries(test_ts_2_col), expected_test_ts_2_col)
+    assert array_equality_test_helper(validate_timeseries(test_ts1), test_ts1)
+    assert array_equality_test_helper(validate_timeseries(test_ts_2_col), expected_test_ts_2_col)
