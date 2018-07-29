3cb4753555f068824617052c7be4df21e7cde68b
Damien Robertson <robertsondamien@gmail.com>
2018-07-28 23:33:43 -0400

    Update repo testing options

--- setup.py
+++ setup.py
@@ -26,11 +26,9 @@ setuptools.setup(
         "Development Status :: 1 - Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
     ],
 )
